3758637cc2b1ae51e7d25d15c18679bbe00eb516
Marcin Owsiany <porridge@debian.org>
2007-03-17 22:31:14 +0000

    first stab at merge_diff refactoring to make it easier to add new collections, and possibly make it easier to understand its logic
    git-svn-id: svn://svn.debian.org/cruft/branches/redesign-200702@142 14f89510-8efc-0310-927e-d869d0d96f2b

--- merge_diff_funcs.c
+++ merge_diff_funcs.c
@@ -5,13 +5,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdarg.h>
 
 #define TRUE (1)
 #define FALSE (0)
 
-#define EXP (0)
-#define FIL (1)
-#define NEE (2)
+#define EXPL_IDX (0)
+#define FILE_IDX (1)
+#define NEED_IDX (2)
+
+#define NUM_INPUT (3)
+static const int contestants[] = { -1, EXPL_IDX, FILE_IDX, NEED_IDX };
+
+#define DEBUG_CRUFT 0
 
 typedef struct fn_stream {
     FILE* file;
@@ -19,11 +25,34 @@ typedef struct fn_stream {
 } fn_stream;
 
 typedef struct collection {
-    fn_stream* s[100];
+    char*      name;
     int        n;
+    int        least;
+    fn_stream* in[100];
+    FILE*      out[100];
+    char*      input_prefix;
+    char*      output_prefix;
 } collection;
 
-void next_line( fn_stream* s ) {
+static void cruft_debug(const char *fmt, ...)
+{
+#if DEBUG_CRUFT > 1
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+#endif
+}
+
+static char *least_line(collection *c)
+{
+	if (c->least < 0)
+		return "(eof)";
+	else
+		return c->in[c->least]->line;
+}
+
+static void next_line( fn_stream* s ) {
     assert(s != NULL);
     assert(s->file != NULL);
 
@@ -36,7 +65,12 @@ void next_line( fn_stream* s ) {
     }
 }
 
-fn_stream* init_fn_stream( char* filename ) {
+static void scroll(collection *c)
+{
+	next_line(c->in[c->least]);
+}
+
+static fn_stream* init_fn_stream( char* filename ) {
     fn_stream* s = malloc( sizeof( fn_stream ) );
     if ( !s ) return NULL;
 
@@ -51,184 +85,240 @@ fn_stream* init_fn_stream( char* filename ) {
     return s;
 }
 
-int find_least( collection* c ) {
-    int least = -1;
+static int find_least( collection* c ) {
     int i;
+    c->least = -1;
 
     for ( i = 0; i < c->n; i++ ) {
-	if ( NULL == c->s[i]->file ) {
+	if ( NULL == c->in[i]->file ) {
 	    continue;
-	} else if ( least == -1 ) { 
-	    least = i;
+	} else if ( c->least == -1 ) { 
+	    c->least = i;
 	} else {
-	    int x = strcmp( c->s[least]->line, c->s[i]->line );
+	    int x = strcmp( c->in[c->least]->line, c->in[i]->line );
 	    if ( x == 0 ) {
-		next_line(c->s[i]);
+		next_line(c->in[i]);
 	    } else if ( x > 0 ) {
-		least = i;
+		c->least = i;
 	    }
 	}
     }
 
-    return least;
+    return c->least;
 }  
 
-void close_collection( collection* c ) {
-    int i;
-    
-    for ( i = 0; i < c->n; i++ ) {
-	if ( c->s[i]->file ) fclose( c->s[i]->file );
-	free(c->s[i]);
-    }
+static void close_collection( collection* c )
+{
+	int i;
 
-    c->n = 0;
+	for (i = 0; i < c->n; i++)
+	{
+		if (c->out[i])
+			fclose(c->out[i]);
+		if (c->in[i]->file)
+			fclose(c->in[i]->file);
+		free(c->in[i]);
+	}
+	c->n = 0;
+}
+
+static int compare(int champion, int challenger, collection *collections)
+{
+	int champion_has_lines;
+	int challenger_has_lines;
+
+	if (champion == -1) {
+		/* can only win if the challenger has no more lines */
+		int winner = collections[challenger].least < 0 ? champion : challenger;
+		cruft_debug("\t\t\t\tFake champion, winner = %d\n", winner);
+		return winner;
+	}
+
+	cruft_debug("\t\t\t\tComparing champion=%d and challenger=%d\n", champion, challenger);
+	
+	champion_has_lines = collections[champion].least >= 0;
+	challenger_has_lines = collections[challenger].least >= 0;
+
+#define A_TIE -2
+
+	if (champion_has_lines && challenger_has_lines) {
+		int res = strcmp(least_line(&collections[champion]), least_line(&collections[challenger]));
+		cruft_debug("\t\t\t\tBoth have lines: %s\n", res < 0 ? "champion wins" : res > 0 ? "challenger wins" : "a tie");
+		return res < 0 ? champion : res > 0 ? challenger : A_TIE;
+	} else if (challenger_has_lines) {
+		cruft_debug("\t\t\t\tOnly challenger has lines, so wins\n");
+		return challenger;
+	} else if (champion_has_lines) {
+		cruft_debug("\t\t\t\tOnly champion has lines, so wins\n");
+		return champion;
+	} else {
+		cruft_debug("\t\t\t\tNone have lines, a tie\n");
+		return A_TIE;
+	}
 }
 
 
-int merge_diff(char *spool_dir_name)
+
+/*
+ * Opens an input file for reading, and an output file (with a changed prefix)
+ * for writing.
+ * TODO: because of how the filename is transformed, we are currently limited
+ * to 4-char prefixes for both input and output filenames.
+ */
+static void open_streams(char *d_name, collection *c)
 {
-    collection expl;
-    FILE*      miss[100];
-    collection file;
-    FILE*      unex[100];
-    collection need;
-    FILE*      want[100];
-    int i;
-    char filename[1000];
+	/* XXX buffer overflow */
+	char filename[1000];
+	strcpy(filename, d_name);
+	strncpy(filename, c->output_prefix, strlen(c->output_prefix));
+	c->out[c->n] = fopen(filename, "w" );
+	c->in[c->n] = init_fn_stream(d_name);
+	c->n++;
+}
 
-    DIR* spool_dir;
-    struct dirent* pde;
+/* have all collections been read completly? */
+static int any_input_has_lines(collection *input)
+{
+	unsigned int idx;
+	int input_present = 0;
+	for (idx = 0; idx < NUM_INPUT; idx++)
+		if (input[idx].least >= 0)
+			input_present = 1;
+	return input_present;
+}
 
-    expl.n = 0;
-    file.n = 0;
-    need.n = 0;
-  
-    spool_dir = opendir( spool_dir_name );
-    if ( spool_dir == NULL ) {
-	perror( spool_dir_name );
-	return 1;
-    }
+static void scroll_winners(int *winners, collection *input)
+{
+	unsigned int idx;
+	for (idx = 0; idx < NUM_INPUT; idx++)
+		if (winners[idx])
+			scroll(&input[idx]);
+}
 
-    chdir( spool_dir_name );
-    while( (pde = readdir( spool_dir )) ) {
-	if ( strncmp( pde->d_name, "expl_", 5 ) == 0 ) {
-	    strcpy( filename, pde->d_name );
-	    strncpy( filename, "miss", 4 );      
-	    miss[expl.n] = fopen( filename, "w"  );
-	    expl.s[expl.n++] = init_fn_stream( pde->d_name );
-	} else if ( strncmp( pde->d_name, "file_", 5 ) == 0 ) {
-	    strcpy( filename, pde->d_name );
-	    strncpy( filename, "unex", 4 );      
-	    unex[file.n] = fopen( filename, "w" );
-	    file.s[file.n++] = init_fn_stream( pde->d_name );
-	} else if ( strncmp( pde->d_name, "need_", 5 ) == 0 ) {
-	    strcpy( filename, pde->d_name );
-	    strncpy( filename, "want", 4 );      
-	    want[need.n] = fopen( filename, "w" );
-	    need.s[need.n++] = init_fn_stream( pde->d_name );
-	}
-    }
-    closedir(spool_dir);
-    
-    for(;;) {
-	int least_expl = find_least( &expl );
-	int least_file = find_least( &file );
-	int least_need = find_least( &need );
-	int has[3];
-	int y;
 
-	if ( least_expl < 0 && least_file < 0 && least_need < 0 ) break;
+static void close_collections(collection *input)
+{
+	unsigned int idx;
+	for (idx = 0; idx < NUM_INPUT; idx++)
+		close_collection(&input[idx]);
+}
+static void prepare_round(collection *input)
+{
+	unsigned int idx;
+	for (idx = 0; idx < NUM_INPUT; idx++)
+		find_least(&input[idx]);
+}
 
-	has[EXP] = least_expl >= 0;
-	has[FIL] = least_file >= 0;
-	has[NEE] = least_need >= 0;
+static void output_line(collection *c)
+{
+	fprintf(c->out[c->least], "%s\n", least_line(c));
+}
 
-	if ( has[EXP] && has[FIL] ) {
-	    y = strcmp( expl.s[least_expl]->line, file.s[least_file]->line );
-	    if ( y < 0 ) {
-		has[FIL] = FALSE;
-		if ( has[NEE] ) {
-		    y = strcmp( expl.s[least_expl]->line, 
-				need.s[least_need]->line );
-		    if ( y < 0 ) {
-			has[NEE] = FALSE;
-		    } else if ( y > 0 ) {
-			has[EXP] = FALSE;
-		    }
-		}   
-	    } else if ( y > 0 ) {
-		has[EXP] = FALSE;
-		if ( has[NEE] ) {
-		    y = strcmp( file.s[least_file]->line, 
-				need.s[least_need]->line );
-		    if ( y < 0 ) {
-			has[NEE] = FALSE;
-		    } else if ( y > 0 ) {
-			has[FIL] = FALSE;
-		    }
-		}
-	    } else {
-		if ( has[NEE] ) {
-		    y = strcmp( expl.s[least_expl]->line, 
-				need.s[least_need]->line );
-		    if ( y < 0 ) {
-			has[NEE] = FALSE;
-		    } else if ( y > 0 ) {
-			has[EXP] = FALSE;
-			has[FIL] = FALSE;
-		    }
-		}
-	    }
+static int open_files(char *spool_dir_name, collection *input)
+{
+	DIR* spool_dir;
+	struct dirent* pde;
+
+	spool_dir = opendir( spool_dir_name );
+	
+	if ( spool_dir == NULL ) {
+		perror( spool_dir_name );
+		return 0;
 	}
 
-	assert(has[EXP] || has[FIL] || has[NEE]);
+	if (chdir(spool_dir_name) != 0) {
+		perror(spool_dir_name);
+		return 0;
+	}
 
-	assert( !has[EXP] || least_expl >= 0 );
-	assert( !has[FIL] || least_file >= 0 );
-	assert( !has[NEE] || least_need >= 0 );
-
-
-#if 0
-fprintf( stderr, "%c%c%c %s (%s %s %s)\n", 
-	 has[EXP] ? '*' : '-',
-	 has[FIL] ? '*' : '-',
-	 has[NEE] ? '*' : '-',
-	 has[EXP] ? expl.s[least_expl]->line : 
-	     has[FIL] ? file.s[least_file]->line :
-                 need.s[least_need]->line,
-	 least_expl >= 0 ? expl.s[least_expl]->line : "(eof)",
-	 least_file >= 0 ? file.s[least_file]->line : "(eof)",
-	 least_need >= 0 ? need.s[least_need]->line : "(eof)"
-	 );
-#endif
-
-	if ( has[EXP] && !has[FIL] ) { /* explained, but not there */
-	    fprintf( miss[least_expl], "%s\n", expl.s[least_expl]->line );
+	while( (pde = readdir( spool_dir )) ) {
+		unsigned int idx;
+		for (idx = 0; idx < NUM_INPUT; idx++) {
+			collection *c = &input[idx];
+			if (strncmp(pde->d_name, c->input_prefix, strlen(c->input_prefix)) == 0)
+				open_streams(pde->d_name, c);
+		}
 	}
 	
-	if ( has[FIL] && !has[EXP] ) { /* there, but not explained */
-	    fprintf( unex[least_file], "%s\n", file.s[least_file]->line ); 
+	closedir(spool_dir);
+	
+	return 1;
+}
+
+static void contest(int *winners, collection *input)
+{
+	int champion = *contestants;
+	int challenger;
+	unsigned int current;
+	for (current = 1, challenger = contestants[current]; current < sizeof(contestants)/sizeof(int); challenger = contestants[++current]) {
+		int winner = compare(champion, challenger, input);
+		if (winner == champion)
+			; /* nothing changes */
+		else if (winner == challenger) {
+			memset(winners, 0, sizeof(int) * NUM_INPUT);
+			winners[challenger] = 1;
+			champion = winner;
+		} else
+			/* a tie */
+			winners[challenger] = 1;
 	}
 
-	if ( has[NEE] && !has[FIL] ) { /* needed but not there */
-	    fprintf( want[least_need], "%s\n", need.s[least_need]->line ); 
+	/* some assertions */
+	{
+		unsigned int idx;
+		assert(winners[EXPL_IDX] || winners[FILE_IDX] || winners[NEED_IDX]);
+		for (idx = 0; idx < NUM_INPUT; idx++)
+			assert(! (winners[idx] && input[idx].least < 0));
 	}
+}
 
-	if ( has[EXP] ) next_line( expl.s[least_expl] );
-	if ( has[FIL] ) next_line( file.s[least_file] );
-	if ( has[NEE] ) next_line( need.s[least_need] );
+int merge_diff(char *spool_dir_name)
+{
+	collection input[NUM_INPUT] = {
+		{ "explain", 0, -1, { NULL }, { NULL }, "expl_", "miss" },
+		{ "file",    0, -1, { NULL }, { NULL }, "file_", "unex" },
+		{ "need",    0, -1, { NULL }, { NULL }, "need_", "want" }
+	};
 
-    }
+	if (! open_files(spool_dir_name, input))
+		return 1;
+	
+	cruft_debug("Expl\n" "| File\n" "|/ Need\n" "||/\n" "|||\n");
 
-    for ( i = 0; i < expl.n; i++ ) fclose( miss[i] );
-    close_collection(&expl);
+	for(;;) {
+		int winners[NUM_INPUT] = { 0, 0, 0 };
+		prepare_round(input);
+		
+		if (! any_input_has_lines(input))
+			break;
 
-    for ( i = 0; i < file.n; i++ ) fclose( unex[i] );
-    close_collection(&file);
+		contest(winners, input);
 
-    for ( i = 0; i < need.n; i++ ) fclose( want[i] );
-    close_collection(&need);
+		cruft_debug("%c%c%c %s (%s %s %s)\n",
+			winners[EXPL_IDX] ? '*' : '-',
+			winners[FILE_IDX] ? '*' : '-',
+			winners[NEED_IDX] ? '*' : '-',
+			winners[EXPL_IDX] ? least_line(&input[EXPL_IDX]) : 
+				winners[FILE_IDX] ? least_line(&input[FILE_IDX]) : least_line(&input[NEED_IDX]),
+			least_line(&input[EXPL_IDX]),
+			least_line(&input[FILE_IDX]),
+			least_line(&input[NEED_IDX])
+		);
+	
+		if (winners[EXPL_IDX] && ! winners[FILE_IDX]) /* explained, but not there */
+			output_line(&input[EXPL_IDX]);
+		
+		if (winners[FILE_IDX] && ! winners[EXPL_IDX]) /* there, but not explained */
+			output_line(&input[FILE_IDX]);
+	
+		if (winners[NEED_IDX] && ! winners[FILE_IDX]) /* needed but not there */
+			output_line(&input[NEED_IDX]);
+	
+		scroll_winners(winners, input);
+	}
+	
+	close_collections(input);
 
-    return 0;
+	return 0;
 }
 
